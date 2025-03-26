7c89e0da625d314ee2a1b3a7087239ca9c9dd0d6
YongMin Son <76940705+F91-plus@users.noreply.github.com>
2025-03-26 19:44:56 +0900

    20_03_26_동적계획법

---
+++ Class13th (Dynamic Programing)/Source.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 1024
+#define SIZE2 100001	//C6262
+
+int save_list[SIZE];
+
+int Fibonacci(int n)
+{
+	int* temp = new int[n];
+	int num = 1;
+
+	if (n == 0) return 1;
+
+	temp[0] = num;
+	if (n <= 2)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			temp[i] = temp[i];
+		}
+	}
+	else {
+		temp[1] = temp[0];
+		for (int i = 2; i < n; i++)
+		{
+			temp[i] = temp[i - 2] + temp[i - 1];
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << temp[i] << " ";
+	}
+
+	return 0;
+}
+
+int Fibonacci2(int n)
+{
+	if (n == 0) {
+		return 0;
+	}
+	else if (n <= 2)
+	{
+		return 1;
+	}
+
+	int a = 0, b = 0;
+
+	if (save_list[n - 1] != 0)
+	{
+		a = save_list[n - 1];
+	}
+	if (save_list[n - 2] != 0)
+	{
+		b = save_list[n - 2];
+	}
+
+	if (save_list[n] == 0)
+	{
+		save_list[n] = n;
+	}
+
+	if (a != 0) {
+		return a + Fibonacci2(n - 2);
+	}
+	else if (b != 0)
+	{
+		return Fibonacci2(n - 1) + b;
+	}
+	else
+	{
+		return Fibonacci2(n - 1) + Fibonacci2(n - 2);
+	}
+
+	/*
+		int Fibonacci2(int n, int list[])
+		if(list[n] !=0){
+		return list[n]}
+
+		return list[n] = Fibonacci2(n - 1, list)+Fibonacci2(n - 2, list);
+	*/
+}
+
+int main()
+{
+#pragma region 동적 계획법
+	/*
+		특정 범위까지의 값을 구하기 위해 그것과 다른 범위까지의
+		값을 이용해서 효율적으로 값을 구하는 알고리즘
+
+		(Overlapping Subproblems) 겹치는 부분 문제
+		동일한 작은 문제들이 반복하여 나타나는 경우를 의미
+
+		(Optimal Substructure) 최적 부분 구조
+		부분 문제의 최적 결과 값을 사용하여 전체 문제의 최적
+		결과를 낼 수 있는 경우를 의미
+
+		메모이제이션 (Memoization)
+		프로그램이 동일한 계산을 반복해야 할 떄, 이전에 계산한
+		값을 메모리에 저장함으로써 동일한 계산을 반복 수행하는
+		작업을 제거하여 프로그램의 실행 속도를 향상시키는 방법
+	*/
+#pragma endregion
+
+	int num = 10;
+
+	int list[SIZE2] = { 0, };
+
+	Fibonacci(20);
+	cout << endl;
+	cout << Fibonacci2(20) << endl;
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,89 +2,131 @@
 
 using namespace std;
 
-#define SIZE 1024
-
-int save_list[SIZE];
-
-int Fibonacci(int n) 
+void money_check(int won) 
 {
-	int* temp = new int[n];
-	int num = 1;
+	int won1000 = 1000;
+	int won500 = 500;
+	int won100 = 100;
+	int won50 = 50;
+	int won10 = 10;
 
-	if (n == 0) return 1;
+	int won1000_count = 0;
+	int won500_count = 0;
+	int won100_count = 0;
+	int won50_count = 0;
+	int won10_count = 0;
 
-	temp[0] = num;
-	if (n <= 2)
-	{
-		for (int i = 0; i < n; i++)
+	while (true) {
+		if (won > 1000)
 		{
-			temp[i] = temp[i];
+			won = won - won1000;
+			won1000_count++;
+		}
+		else if (won >= 500 && won < 1000) {
+			won = won - won500;
+			won500_count++;
+		}
+		else if (won >= 100 && won < 500)
+		{
+			won = won - won100;
+			won100_count++;
+		}
+		else if (won >= 50 && won < 100) {
+			won = won - won50;
+			won50_count++;
+		}
+		else if (won >= 10 && won < 50)
+		{
+			won = won - won10;
+			won10_count++;
+		}
+		else if (won <= 9)
+		{
+			break;
 		}
 	}
-	else {
-		temp[1] = temp[0];
-		for (int i = 2; i < n; i++)
-		{
-			temp[i] = temp[i - 2] + temp[i-1];
-		}
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << temp[i] << " ";
-	}
-
-	return 0;
+	cout << "1000원 : " << won1000_count << endl;
+	cout << "500원 : " << won500_count << endl;
+	cout << "100원 : " << won100_count << endl;
+	cout << "50원 : " << won50_count << endl;
+	cout << "10원 : " << won10_count << endl;
 }
 
-int Fibonacci2(int n)
-{
-	if (n == 0) {
-		return 0;
-	}
-	else if (n <= 2)
-	{
-		return 1;
+const int& Greedy(int n) {
+	int count = 0;
+
+	while (n >= 10) {
+		if (n >= 1000)
+		{
+			n -= 1000;
+			count++;
+		}
+		else if (n >= 500)
+		{
+			n -= 500;
+			count++;
+		} 
+		else if (n >= 100) 
+		{
+			n -= 100;
+			count++;
+		}
+		else if (n >= 50) {
+			n -= 50;
+			count++;
+		}
+		else if (n >= 10) {
+			n -= 10;
+			count++;
+		}
 	}
 
-	if (save_list[n] != 0) 
-	{	
-		return Fibonacci2(save_list[n]);
-	}
-
-	if (save_list[n] == 0)
-	{
-		//save_list[n] = n;
-	}
-	return Fibonacci2(n - 1) + Fibonacci2(n - 2);
+	return count;
 }
 
 int main()
 {
-#pragma region 동적 계획법
+#pragma region 탐욕법
 	/*
-		특정 범위까지의 값을 구하기 위해 그것과 다른 범위까지의
-		값을 이용해서 효율적으로 값을 구하는 알고리즘
+		최적의 해를 구하는 데에 사용되는 근사적인 방법으로 여러 경우 중
+		하나를 검색해야 할 떄마다 그 순간에 최적이라고 생각되는 것을
+		선택해 나가는 방식으로 진행하여 최종적인 해답을 구하는 알고리즘
 
-		(Overlapping Subproblems) 겹치는 부분 문제
-		동일한 작은 문제들이 반복하여 나타나는 경우를 의미
+		1. 탐욕 선택 속성
+		각 단계에서 '최적의 선택'을 했을 때 전체 문제에 대한 
+		최적의 해를 구할 수 있는 경우
 
-		(Optimal Substructure) 최적 부분 구조
-		부분 문제의 최적 결과 값을 사용하여 전체 문제의 최적
-		결과를 낼 수 있는 경우를 의미
+		2. 최적 부분 구조
+		전체 문제의 최적의 해가 '부분 문제의 최적의 해로 구성
+		될 수 있는 경우
 
-		메모이제이션 (Memoization)
-		프로그램이 동일한 계산을 반복해야 할 떄, 이전에 계산한
-		값을 메모리에 저장함으로써 동일한 계산을 반복 수행하는
-		작업을 제거하여 프로그램의 실행 속도를 향상시키는 방법
+		그리디 알고리즘 단계 (요약본?)
+		1. 선택 절차(Selection Procedure)
+			- 현재 상태에서의 최적의 해답을 선택한다.
+		2. 적절성 검사 (Feasibility)
+			- 선택된 해가 문제의 조건을 만족하는지 검사.
+		3. 해답 검사 (Solution Check)
+			- 원래의 문제가 해결되었는지 검사하고, 해결되지 않았다면
+			  선택 절차로 돌아가 위의 과정을 반복한다.
+
+		그리디 알고리즘 단계 (상세내용)
+		1. 문제의 최적 부분 구조를 결정
+		2. 문제의 구조에 맞게 선택 절차를 정의함
+		3. 선택 절차에 따라 선택을 수행함
+		4. 선택된 해가 문제의 조건을 만족하는 지 검사
+		5. 조건을 만족하지 않으면 해당 해를 제외한다.
+		6. 모든 선택이 완료되면 해답을 검사한다.
+		7. 조건을 만족하지 않으면 해답으로 인정되지 않는다.
+
 	*/
 #pragma endregion
+	int money = 1370;
 
-	int num = 50;
+	money_check(money);
 
-	//Fibonacci(num);
+	cout << endl;
 
-	//cout<<Fibonacci2(50)<<endl;	// 계산이 1분 넘음
-	cout<<Fibonacci2(5)<<endl;		// 
+	cout << Greedy(money);
+
 	return 0;
 }
