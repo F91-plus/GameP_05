fd3f7a7daa40763ce8c00c924eac8c181e0493fa
YongMin Son <plusf91@gmail.com>
2025-04-03 19:44:08 +0900

    25_04_03_집합

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,6 +18,14 @@ private:
 			this->vertexX = vertexX;
 			this->vertexY = vertexY;
 			this->weight = weight;
+		}
+
+		const int& VertexX() { return vertexX; }
+		const int& VertexY() { return vertexY; }
+		const int& Weight() { return weight; }
+
+		const bool& operator < (const Edge& edge) {
+			return weight < edge.weight;
 		}
 	};
 	int cost;
@@ -37,7 +46,91 @@ public:
 		//Edge edge(vertexX, vertexY, weight);
 		nodeList.push_back(Edge(vertexX, vertexY, weight));	//..k(edge);
 	}
-	
+
+	int find(int x)
+	{
+		//배열의 인덱스와 값이 같다면 Root Node 발견
+		if (x == parent[x]) {
+			return x;
+		}
+		else {
+			// 부모 노드의 번호를 전달하면서, Root Node를 찾을 떄까지
+			// 재귀 호출을 반복한다.
+			return parent[x] = find(parent[x]);
+		}
+	}
+
+	void Union(int x, int y)
+	{
+		x = find(x);
+		y = find(y);
+
+		if (x == y) return;
+
+		if (x < y)
+		{
+			parent[y] = x;
+		}
+		else {
+			parent[x] = y;
+		}
+	}
+
+	bool same(int x, int y) {
+		return find(x) == find(y);
+	}
+
+	void calculate() {
+		sort(nodeList.begin(), nodeList.end());
+
+		for (int i = 0; i < nodeList.size(); i++)
+		{
+			if (same(nodeList[i].VertexX(), nodeList[i].VertexY()) != 1) {
+				cost += nodeList[i].Weight();
+
+				Union(nodeList[i].VertexX(), nodeList[i].VertexY());
+			}
+		}
+
+		cout << "최소 Cost : " << cost << endl;
+		
+		/*while (true)
+		{			
+			int count = 0;
+			if (nodeList[0].Weight() < nodeList[1].Weight())
+			{
+				Union(nodeList[0].VertexX(), nodeList[0].VertexY());
+				count++;
+				if (same(nodeList[0].VertexX(), nodeList[0].VertexY()) != 1)
+				{
+					parent[0] = nodeList[0].Weight();
+				}
+			}
+			else
+			{
+				count++;
+				parent[0] = nodeList[1].Weight();
+			}
+			
+			if (count == 6)
+			{
+				break;
+			}
+		}*/
+
+
+		/*for (int i = 0; i < nodeList.size(); i++)
+		{
+			cout << "vertexX : " << nodeList[i].VertexX() << endl;
+			cout << "vertexY : " << nodeList[i].VertexY() << endl;
+			cout << "wieght : " << nodeList[i].Weight() << endl;
+			cout << endl;
+		}*/
+
+	}
+
+
+
 };
 
 int main()
@@ -70,5 +163,6 @@ int main()
 	kruskal.insert(5, 6, 48);
 	kruskal.insert(3, 6, 36);
 		
+	kruskal.calculate();
 	return 0;
 }
